e822f9512db2895389bc95b26b35859d4b2aecc4
Cat Whitmer <catwhitmer@gmail.com>
2021-03-04 09:16:59 -0500

    query selected all needed DOM elements

--- quiz/settings.js
+++ quiz/settings.js
@@ -1,5 +1,20 @@
 class Settings {
-    constructor() {   
+    constructor() {  
+        this.quizElement = document.querySelector('.quiz');
+        this.settingsElement = document.querySelector('.settings'); 
+        this.category = document.querySelector('#category');
+        this.numberOfQuestions = document.querySelector('#questions');
+        this.difficulty = [
+            document.querySelector('#easy'),
+            document.querySelector('#medium'),
+            document.querySelector('#hard'),
+        ];
+        
+        this.startButton = document.querySelector('#start');
+
+        this.quiz = {};
+
+        this.startButton.addEventListener('click', this.startQuiz.bind(this));
     }
 
     startQuiz() {
